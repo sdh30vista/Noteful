93a74c1d51ab1137c086facd9c050da396463c92
sdh30vista <s.deane.healey@gmail.com>
2016-02-05 06:48:10 +0000

    These are a bunch of changes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,10 @@
 class Note < ActiveRecord::Base
+  
+  def preview
+    self.content.slice(0..100) << ' ...(read more)'
+  end
+  
   belongs_to :user
-
   validates :title, presence: true
   validates :content, presence: true
 
